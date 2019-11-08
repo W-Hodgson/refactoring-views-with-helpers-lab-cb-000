4b25b645f462caeeb5dc79bc29d5079bd7fc8908
Will Hodgson <will.hodgson@current-rms.com>
2019-11-08 15:48:15 +0000

    Automatically backed up by Learn

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -3,7 +3,7 @@ module SongsHelper
     if song.artist
       link_to "#{song.artist.name}", artist_path(song.artist)
     else
-      link_to "Add Artist"
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 end
